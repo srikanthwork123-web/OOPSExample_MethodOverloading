d39918f9e57c8a9398673d98d028cb50d7f53e31
Dell <Dell@DESKTOP-13B42NJ>
2026-01-29 09:05:43 +0530

    Add project files.

---
+++ OOPSExample_MethodOverloading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //==========constuctor overloading start here======
+            a obj1 = new a();//it will call parameter less constructor(userdefined default consturctor)
+            a obj2 = new a(10);//it will call the single parameter constuctor
+            a obj3 = new a(10, 10);//it will call the Two parameter constuctor
+            a obj4 = new a(10, 10, "av");//it will call the Three parameter constuctor((int a, int b,string c) this combination)
+            a obj5 = new a(10, 10, "a");//it will call the Three parameter constuctor((int a, float b, string c) this combination)
+
+            //==========constuctor overloading End here======
+
+
+            //=============Method overloading start here============
+            a obj = new a(10,10,"abc");
+            obj.add("a", "b", 10);//here your c# compiler deciside which method need to call based ur on your parameter.
+            obj.add();//it will call the without parameter method
+            obj.add();//we can call same method multiple times also.it will not throw any error.
+            obj.add(10);//it will call the single parameter method
+            obj.add(10, 10);//it will call two paarmeter(int,int )combination method it willcall
+            obj.add("a", "b");
+            obj.add("a", "b", 10, 1);
+            obj.add(10, 10, "a");
+            obj.add("a1", "b1", 10);
+            obj.add("c1", "c2", "c3");
+            //=============Method overloading End here============
+        }
+    }
+}
---
+++ OOPSExample_MethodOverloading/a.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_MethodOverloading
+{
+    //Method overloading: Same method name but different parameters.
+    //Same combination of method should not repeat in method overloading.if it's repeat it will compiletime error.
+    //Same combination of constuctor should not repeat in constructor overloading.if it's repeat it will compiletime error.
+    //if writing the code,if any error is showing we called that error is called "compiletime error";
+    internal class a
+    {
+        /*
+        Method Syntax: -
+        ======================================
+       accessmodifer Returntype methodname(parameters or argments)
+        {
+
+            return data;
+        }
+        */
+        //Void does not return any data.
+        //Except void remaing all return types return the data.
+
+        //============constructor overloading start here=============================
+        //user defined default consturor will be created by user .without parameter.
+        //system defined default constuctor will be created by c#compiler at runtime (while running the program c# compiler will create system defined default constructor)
+        public a()//user defined default constuctor.it does not conatin any parameters
+        {
+            Console.WriteLine("i am default constructor");
+        }
+        public a(int a)
+        {
+            Console.WriteLine("i am single paramaterised(int a) constructor");
+        }
+        public a(int a,int b)
+        {
+            Console.WriteLine("i am Two paramaterised(int a,int b) constructor");
+            //constucor usage is assign the data to instance variables 
+        }
+        public a(int a, int b,string c)
+        {
+            Console.WriteLine("i am Three paramaterised(int a, int b,string c) constructor");
+        }
+        public a(int a, float b, string c)
+        {
+            Console.WriteLine("i am Three paramaterised(int a, float b, string c) constructor");
+        }
+        //============constructor overloading end here=============================
+        //============method overloading start here=============================
+        //method usage is perform some opertions on varaibles and return data and excuting logic inside method also
+        public void add()
+        {
+            Console.WriteLine("i am without parameter method");
+        }
+        public void add(int x)
+        {
+            Console.WriteLine("i am single paramaterised(int x) method");
+        }
+        public void add(int x, int y)
+        {
+            Console.WriteLine("i am Two paramaterised(int x, int y) method");
+            int t = x + y;
+            Console.WriteLine(t);
+        }
+        public void add(int x,int y,string a)
+        {
+            Console.WriteLine("i am Three paramaterised(int x,int y,string a) method");
+            int t = x + y;
+            Console.WriteLine(a+t);
+        }
+
+        public void add(string a,string b)
+        {
+            Console.WriteLine("i am Two paramaterised(string a,string b) method");
+            Console.WriteLine(a+b); 
+        }
+        public void add(string a, string b,string c)
+        {
+            Console.WriteLine("i am Three paramaterised(string a, string b,string c) method");
+            Console.WriteLine(a + b);
+        }
+        public void add(string a, string b, float c)
+        {
+            Console.WriteLine("i am Three paramaterised(string a, string b, float c) method");
+            Console.WriteLine(a + b);
+        }
+        public void add(string a, string b, float c,int d)
+        {
+            Console.WriteLine("i am Four paramaterised(string a, string b, float c,int d) method");
+            Console.WriteLine(a + b);
+        }
+        public void add(float c, string a, string b)
+        {
+            Console.WriteLine("i am Three paramaterised(float c, string a, string b) method");
+            Console.WriteLine(a + b);
+        }
+        //============method overloading end here=============================
+    }
+}
